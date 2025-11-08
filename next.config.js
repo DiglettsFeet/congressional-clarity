ce0bf7f43c67e2143d9dbc4011574c434584f0b0
DiglettsFeet <horne.jacob@gmail.com>
2025-11-07 19:43:17 -0500

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  env: {
-    CONGRESS_API_KEY: process.env.CONGRESS_API_KEY
-  }
-}
-
-module.exports = nextConfig;
