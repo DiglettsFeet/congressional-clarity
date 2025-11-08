5282da208fa6fffc55202c47886f4e247da00bbc
DiglettsFeet <horne.jacob@gmail.com>
2025-11-07 19:30:53 -0500

    Update member-votes.js

--- pages/api/member-votes.js
+++ pages/api/member-votes.js
@@ -7,7 +7,9 @@ export default async function handler(req, res) {
     return res.status(400).json({ error: "Missing bioguideId or subject parameter" });
   }
 
-  const voterUrl = \`https://www.govtrack.us/api/v2/vote_voter?person__bioguideid=\${bioguideId}&limit=600\`;
+  const voterUrl = "https://www.govtrack.us/api/v2/vote_voter?person__bioguideid=" +
+                   encodeURIComponent(bioguideId) +
+                   "&limit=600";
 
   try {
     const voterRes = await fetch(voterUrl);
@@ -20,17 +22,20 @@ export default async function handler(req, res) {
       const relatedBill = vote.vote.related_bill;
 
       if (relatedBill && relatedBill.id) {
-        const billUrl = \`https://www.govtrack.us/api/v2/bill/\${relatedBill.id}\`;
+        const billUrl = "https://www.govtrack.us/api/v2/bill/" + relatedBill.id;
         const billRes = await fetch(billUrl);
         const billData = await billRes.json();
 
-        const subjects = billData.subjects.map(s => s.toLowerCase());
+        const subjects = (billData.subjects || []).map(s => s.toLowerCase());
         if (subjects.some(s => s.includes(subject.toLowerCase()))) {
           billVotes.push({
             billNumber: billData.display_number,
             title: billData.title,
             summary: billData.summary_short || "",
-            congressUrl: \`https://www.congress.gov/bill/\${billData.congress}th-congress/\${billData.bill_type}/\${billData.number}\`,
+            congressUrl: "https://www.congress.gov/bill/" +
+                         billData.congress + "th-congress/" +
+                         billData.bill_type + "/" +
+                         billData.number,
             vote: votePosition
           });
         }
