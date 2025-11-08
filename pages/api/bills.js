12993abf91fcb8fcffc9a93e6cb90eb628bf32b5
DiglettsFeet <horne.jacob@gmail.com>
2025-11-07 19:42:34 -0500

    Delete pages directory

--- pages/api/bills.js
+++
@@ -1,8 +0,0 @@
-export default async function handler(req, res) {
-  const { subject } = req.query;
-
-  const response = await fetch(`https://api.congress.gov/v3/bill?subject=${subject}&format=json&api_key=${process.env.CONGRESS_API_KEY}`);
-  const data = await response.json();
-
-  res.status(200).json(data);
-}
